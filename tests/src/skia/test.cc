4db5aecba19e8ee72c08074e518457e19ba0a602
Clark Rinker <cgrinker@gmail.com>
2022-01-07 15:58:24 -0800

    Adding software renderer path

--- tests/src/skia/test.cc
+++ tests/src/skia/test.cc
@@ -9,13 +9,16 @@
 #include <GLFW/glfw3.h>
 
 #define MSAA_LEVEL 1
+#define WIDTH 640
+#define HEIGHT 480
 
-int main(int argc, char** argv)
-{	
+
+int initglfw()
+{
     GLFWwindow* window;
 
     if (!glfwInit()) {
-        SkDebugf("Failed to Init GLFW\n");
+        
         return -1;
     }
 
@@ -28,31 +31,55 @@ int main(int argc, char** argv)
 	//glfwWindowHint(GLFW_ALPHA_BITS, 0);
 	glfwWindowHint(GLFW_DEPTH_BITS, 0);
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Hello World", NULL, NULL);
+
+    glfwMakeContextCurrent(window);
     if (!window)
     {
         glfwTerminate();
         SkDebugf("Failed to Create Test Window\n");
         return -1;
+    
     }
+    return 0;
+}
 
-    glfwMakeContextCurrent(window);
+int main(int argc, char** argv)
+{	
+    
+    if(!initglfw())
+    {
+        SkDebugf("Testing GPU Integration \n");
+        sk_sp<GrDirectContext> context = GrDirectContext::MakeGL();
+        SkImageInfo info = SkImageInfo::MakeN32Premul(WIDTH, HEIGHT);
+        SkSurfaceProps props;
 
+        sk_sp<SkSurface> gpuSurface(
+                SkSurface::MakeRenderTarget(context.get(), SkBudgeted::kNo, info, MSAA_LEVEL, nullptr));
 
+        if (!gpuSurface) {
+            SkDebugf("SkSurface::MakeRenderTarget returned null\n");
+            return -1;
+        }
+
+        return 0;
+    }
+    else
+    {
+        SkDebugf("Failed to Init GLFW. Falling back to Software Renderer");
+        sk_sp<SkSurface> rasterSurface =
+            SkSurface::MakeRasterN32Premul(WIDTH, HEIGHT);
+        SkCanvas* rasterCanvas = rasterSurface->getCanvas();
+
+        if(rasterCanvas == nullptr)
+        {
+            SkDebugf("Failed to Create Software Renderer");
+            return -1;
+        }
+
+    }
 
     
-    sk_sp<GrDirectContext> context = GrDirectContext::MakeGL();
-    SkImageInfo info = SkImageInfo::MakeN32Premul(640, 480);
-    SkSurfaceProps props;
-
-    sk_sp<SkSurface> gpuSurface(
-            SkSurface::MakeRenderTarget(context.get(), SkBudgeted::kNo, info, MSAA_LEVEL, nullptr));
-
-    if (!gpuSurface) {
-        SkDebugf("SkSurface::MakeRenderTarget returned null\n");
-        return -1;
-    }
-
 
 
 	//sk_sp<SkSurface> gpuSurface(SkSurface::MakeRenderTarget(context, SkBudgeted::kNo, info));
